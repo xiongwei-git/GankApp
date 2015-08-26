4b6f8858d7964edcb54ff92b286568572f53e728
Ted <ted@lifang.com>
2015-08-26 19:33:23 +0800

    add service

---
+++ app/src/main/java/com/android/ted/gank/db/Image.java
@@ -0,0 +1,128 @@
+package com.android.ted.gank.db;
+
+import io.realm.Realm;
+import io.realm.RealmObject;
+import io.realm.RealmResults;
+
+/**
+ * Created by Ted on 2015/8/26.
+ */
+public class Image extends RealmObject{
+    /**补充数据*/
+    private int width = 0;
+    private int height = 0;
+    private int position;
+
+    /**原始数据*/
+    private String who;
+    private String publishedAt;
+    private String desc;
+    private String type;
+    private String url;
+    private boolean used;
+    private String objectId;
+    private String createdAt;
+    private String updatedAt;
+
+    public boolean isItemInDb(Realm realm){
+        RealmResults<Image> results =  realm.where(Image.class).equalTo("objectId",getObjectId()).findAll();
+        return results.size() > 0;
+    }
+
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public String getWho() {
+        return who;
+    }
+
+    public void setWho(String who) {
+        this.who = who;
+    }
+
+    public String getPublishedAt() {
+        return publishedAt;
+    }
+
+    public void setPublishedAt(String publishedAt) {
+        this.publishedAt = publishedAt;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public boolean isUsed() {
+        return used;
+    }
+
+    public void setUsed(boolean used) {
+        this.used = used;
+    }
+
+    public String getObjectId() {
+        return objectId;
+    }
+
+    public void setObjectId(String objectId) {
+        this.objectId = objectId;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
---
+++ app/src/main/java/com/android/ted/gank/service/ImageImproveService.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright 2015 XiNGRZ <chenxingyu92@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.ted.gank.service;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
+
+import com.android.ted.gank.db.Image;
+import com.orhanobut.logger.Logger;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
+
+/**
+ * 完善图片信息的服务
+ *
+ * @author Ted
+ */
+public class ImageImproveService extends IntentService{
+
+    public static final String ACTION_UPDATE_RESULT = "com.android.ted.UPDATE_RESULT";
+
+    public static final String EXTRA_CHANGE = "change_data";
+    public static final String EXTRA_ACTION = "action_name";
+
+    public static final String PERMISSION_ACCESS_UPDATE_RESULT = "com.android.ted.ACCESS_UPDATE_RESULT";
+
+    public static final String ACTION_IMPROVE_IMAGE = "com.android.ted.IMPROVE_IMAGE";
+
+
+    private static final String TAG = "ImageImproveService";
+
+    private final OkHttpClient client = new OkHttpClient();
+
+    public ImageImproveService() {
+        super(TAG);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Realm realm = Realm.getInstance(this);
+
+        /**查询所有尺寸为0的图片*/
+        RealmResults<Image> newImage = realm.where(Image.class).equalTo("width",0)
+                .findAllSorted("position", RealmResults.SORT_ORDER_DESCENDING);
+
+        int change = 0;
+
+        if (null == newImage || newImage.isEmpty()) {
+            Logger.d("no new image, fresh fetch");
+            change = 0;
+        } else if (ACTION_IMPROVE_IMAGE.equals(intent.getAction())) {
+            improveImageInfo(newImage,realm);
+        }
+
+        realm.close();
+
+        Logger.d("finished improve num:" + change);
+
+        Intent broadcast = new Intent(ACTION_UPDATE_RESULT);
+        broadcast.putExtra(EXTRA_CHANGE, change);
+        broadcast.putExtra(EXTRA_ACTION, intent.getAction());
+        sendBroadcast(broadcast, PERMISSION_ACCESS_UPDATE_RESULT);
+    }
+
+    private int improveImageInfo(RealmResults<Image> newImage,Realm realm){
+        int count = 0;
+        for (Image image:newImage){
+            if(saveToDb(realm,image))
+                count++;
+        }
+        return count;
+    }
+
+
+    /**
+     * 预解码图片并将抓到的数据保存至数据库
+     *
+     * @param realm   Realm 实例
+     * @param image 图片
+     * @return 是否保存成功
+     */
+    private boolean saveToDb(Realm realm, Image image) {
+        realm.beginTransaction();
+        try {
+            Point size = new Point();
+            loadImageForSize(image.getUrl(),size);
+            image.setHeight(size.y);
+            image.setWidth(size.x);
+            realm.copyToRealm(image);
+        } catch (IOException e) {
+            Logger.d("Failed to fetch image");
+            realm.cancelTransaction();
+            return false;
+        }
+        realm.commitTransaction();
+        return true;
+    }
+
+    /***
+     * 加载图片内容计算图片大小
+     * @param url
+     * @param measured
+     * @throws IOException
+     */
+    public void loadImageForSize(String url, Point measured) throws IOException {
+        Response response = client.newCall(new Request.Builder().url(url).build()).execute();
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeStream(response.body().byteStream(), null, options);
+        measured.x = options.outWidth;
+        measured.y = options.outHeight;
+    }
+
+}
